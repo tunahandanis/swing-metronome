2f6ec70b8be0d5ef1a0ba471e919903586eda9a8
Tunahan Danış <tunahandanis@outlook.com>
2022-01-17 18:22:49 +0300

    add markup for MetronomeInterface.js

--- src/components/App/MetronomeInterface/MetronomeInterface.js
+++ src/components/App/MetronomeInterface/MetronomeInterface.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const MetronomeInterface = () => {
-  return <div>interface</div>;
+  return (
+    <section>
+      <h1>
+        <span>60</span>BPM
+      </h1>
+      <div>
+        <button>&ndash;</button>
+        <input type="range" min={30} max={250} />
+        <button>+</button>
+      </div>
+      <button>Start/Stop</button>
+    </section>
+  );
 };
 
 export default MetronomeInterface;
