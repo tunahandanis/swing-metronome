3ccc1b5c71060b0e25b845ea68add091fdb83bc3
Tunahan Danış <tunahandanis@outlook.com>
2022-01-17 18:24:52 +0300

    add NotationControl.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import "../../assets/styles/App.css";
 import MetronomeInterface from "./MetronomeInterface/MetronomeInterface";
+import NotationControl from "./NotationControl/NotationControl";
 
 function App() {
   return (
     <main>
       <MetronomeInterface />
+      <NotationControl />
     </main>
   );
 }
