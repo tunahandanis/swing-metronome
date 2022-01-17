6fcbe6a0cc26bfeb82a6ce87e634b28061bd45da
Tunahan Danış <tunahandanis@outlook.com>
2022-01-17 18:48:11 +0300

    add comments in context.js

--- src/context/context.js
+++ src/context/context.js
@@ -9,19 +9,28 @@ import {
 const MetronomeContext = React.createContext();
 
 const MetronomeProvider = ({ children }) => {
+  // STATE HOOKS
   const [isRunning, setIsRunning] = useState(false);
   const [tempo, setTempo] = useState(60);
   const [subdivision, setSubdivision] = useState("Quarter");
   const [barLength, setBarLength] = useState(2);
   const [isStressing, setIsStressing] = useState(false);
 
+  // REF HOOKS
+
   const intervalRef = useRef();
   const audioContext = useRef(null);
   const tempoRef = useRef(tempo);
 
+  // GLOBAL/TEMPORARY VARIABLES
+
+  // These can reset between re-renders, no problem
   let timeBetweenBeats;
+
+  // These are for adjusting subdivision notes and stressed notes, counting the queue
   let currentSubNote = 0;
   let currentQuarterNote = 0;
+
   let nextNoteTime = 0.0;
 
   return <MetronomeContext.Provider>{children}</MetronomeContext.Provider>;
