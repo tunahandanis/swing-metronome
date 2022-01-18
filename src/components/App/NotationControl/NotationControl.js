a046af5ff45bb93556f3b57a1c20b5c07de5cefb
Tunahan Danış <tunahandanis@outlook.com>
2022-01-18 21:59:09 +0300

    add logic to NotationControl.js

--- src/components/App/NotationControl/NotationControl.js
+++ src/components/App/NotationControl/NotationControl.js
@@ -11,29 +11,39 @@ const NotationControl = () => {
     subdivision,
   } = useMetronomeContext();
 
+  // FUNCTIONS FOR INCREASING/DECREASING BEAT NUMBER IN BAR
+
+  const beatUp = () => {
+    if (barLength < 12) setBarLength((prev) => prev + 1);
+  };
+
+  const beatDown = () => {
+    if (barLength > 2) setBarLength((prev) => prev - 1);
+  };
+
   return (
     <section>
       <ul>
         <li>
-          <button>Quarter</button>
+          <button onClick={() => setSubdivision("Quarter")}>Quarter</button>
         </li>
         <li>
-          <button>Eighth</button>
+          <button onClick={() => setSubdivision("Eighth")}>Eighth</button>
         </li>
         <li>
-          <button>Sixteenth</button>
+          <button onClick={() => setSubdivision("Sixteenth")}>Sixteenth</button>
         </li>
         <li>
-          <button>Triple</button>
+          <button onClick={() => setSubdivision("Triplet")}>Triplet</button>
         </li>
       </ul>
 
       <div>
-        <button>Stress</button>
+        <button onClick={() => setIsStressing((prev) => !prev)}>Stress</button>
         <div>
-          <button>-</button>
-          <h3>Bar Length</h3>
-          <button>+</button>
+          <button onClick={beatDown}>-</button>
+          <h3>{barLength}</h3>
+          <button onClick={beatUp}>+</button>
         </div>
       </div>
     </section>
