064acc2310acf67d4ad3025d90b633bfcbf418bb
Tunahan Danış <tunahandanis@outlook.com>
2022-01-22 21:00:03 +0300

    add AudioControl markup

--- src/components/App/AudioControl/AudioControl.js
+++ src/components/App/AudioControl/AudioControl.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 const AudioControl = () => {
-  return <section></section>;
+  const drumAudios = [
+    "Snare",
+    "Hi-hat Closed",
+    "Hi-hat Open",
+    "Sticks",
+    "Bass Drum",
+  ];
+  return (
+    <section>
+      <div>
+        <h1>Quarter note sound</h1>
+        <ul>
+          {drumAudios.map((item) => {
+            return (
+              <li key={"Quarter " + item}>
+                <button>{item}</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div>
+        <h1>Sub note sound</h1>
+        <ul>
+          {drumAudios.map((item) => {
+            return (
+              <li key={"Sub " + item}>
+                <button>{item}</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default AudioControl;
