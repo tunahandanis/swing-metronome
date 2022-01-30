b63437b3b35300cdd0204886e873b0734e620ff1
Tunahan Danış <tunahandanis@outlook.com>
2022-01-30 18:58:39 +0300

    modify interface markup

--- src/components/App/MetronomeInterface/MetronomeInterface.js
+++ src/components/App/MetronomeInterface/MetronomeInterface.js
@@ -29,23 +29,30 @@ const MetronomeInterface = () => {
 
   return (
     <section className="interface">
-      <h1 className="interface__tempo-text">{tempo}BPM</h1>
-      <div className="interface__tempo-control">
-        <button onClick={stepDown} className="btn down-btn">
-          &ndash;
-        </button>
-        <input
-          className="slider tempo-slider"
-          ref={rangeRef}
-          type="range"
-          min={30}
-          max={250}
-          onChange={(e) => slideTempo(parseInt(e.target.value))}
-        />
-        <button onClick={stepUp} className="btn up-btn">
-          +
+      <div className="interface__tempo">
+        <h1 className="interface__tempo-text">{tempo}BPM</h1>
+        <div className="interface__tempo-control">
+          <button onClick={stepDown} className="btn down-btn">
+            &ndash;
+          </button>
+          <input
+            className="slider tempo-slider"
+            ref={rangeRef}
+            type="range"
+            min={30}
+            max={250}
+            onChange={(e) => slideTempo(parseInt(e.target.value))}
+          />
+          <button onClick={stepUp} className="btn up-btn">
+            +
+          </button>
+        </div>
+
+        <button onClick={startStop} className="btn start-stop-btn">
+          Start/Stop
         </button>
       </div>
+
       <div className="interface__swing">
         <h1 className="interface__swing-text">
           Swing Percentage: {swingPercentage}%
@@ -58,9 +65,6 @@ const MetronomeInterface = () => {
           onChange={(e) => slideSwing(parseInt(e.target.value))}
         />
       </div>
-      <button onClick={startStop} className="btn start-stop-btn">
-        Start/Stop
-      </button>
     </section>
   );
 };
