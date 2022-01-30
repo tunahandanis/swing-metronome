5edee754dd1045ee6e87bdd01792d36e3e2cf5db
Tunahan Danış <tunahandanis@outlook.com>
2022-01-30 17:32:14 +0300

    add header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import MetronomeInterface from "./MetronomeInterface/MetronomeInterface";
 import NotationControl from "./NotationControl/NotationControl";
 import AudioControl from "./AudioControl/AudioControl";
+import Header from "./Header/Header";
 import { MetronomeProvider } from "../../context/context";
 
 function App() {
   return (
     <main>
       <MetronomeProvider>
+        <Header />
         <MetronomeInterface />
         <NotationControl />
         <AudioControl />
