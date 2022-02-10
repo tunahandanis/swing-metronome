1763ffe2749da4992f3a1ae7969487b52adb1c2c
Tunahan Danış <tunahandanis@outlook.com>
2022-02-11 00:55:33 +0300

    remove header

--- src/components/App/routes/Metronome/Metronome.js
+++ src/components/App/routes/Metronome/Metronome.js
@@ -2,14 +2,12 @@ import React from "react";
 import MetronomeInterface from "./MetronomeInterface/MetronomeInterface";
 import NotationControl from "./NotationControl/NotationControl";
 import AudioControl from "./AudioControl/AudioControl";
-import Header from "./Header/Header";
 import { MetronomeProvider } from "../../../../context/context";
 
 const Metronome = () => {
   return (
     <main>
       <MetronomeProvider>
-        <Header />
         <MetronomeInterface />
         <NotationControl />
         <AudioControl />
